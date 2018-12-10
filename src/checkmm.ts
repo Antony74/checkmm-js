1d7123e6d8f3e896843b2aed978a408a244523be
Antony <akb@akb.me.uk>
2018-12-10 21:21:18 +0000

    Made a very modest start on this port.

---
+++ src/checkmm.ts
@@ -0,0 +1,114 @@
+// Metamath database verifier
+// Antony Bartlett (akb@akb.me.uk)
+//
+// I release this code to the public domain under the
+// Creative Commons "CC0 1.0 Universal" Public Domain Dedication:
+//
+// http://creativecommons.org/publicdomain/zero/1.0/
+//
+// This is a port to TypeScript.  The original C++ program was
+// written by Eric Schmidt and can be found here:
+// http://us.metamath.org/other.html#checkmm
+//
+// This is a standalone verifier for Metamath database files.
+// Run it with a single file name as the parameter.
+//
+// Some notes:
+//
+// The code assumes that the character set is compatible with ASCII.
+//
+// According to the spec, file inclusion commands should not include a file
+// that has already been included. Unfortunately, determing whether two
+// different strings refer to the same file is not easy, and, worse, is
+// system-dependant. This program ignores the issue entirely and assumes
+// that distinct strings name different files. This should be adequate for
+// the present, at least.
+//
+// If the verifier finds an error, it will report it and quit. It will not
+// attempt to recover and find more errors. The only condition that generates
+// a diagnostic message but doesn't halt the program is an incomplete proof,
+// specified by a question mark. In that case, as per the spec, a warning is
+// issued and checking continues.
+//
+// Please let me know of any bugs.
+// https://github.com/Antony74/checkmm-js/issues
+
+const tokens: string[] = [];
+
+const constants: string[] = [];
+
+type Expression = string[];
+
+// The first parameter is the statement of the hypothesis, the second is
+// true iff the hypothesis is floating.
+class Hypothesis {
+  first: Expression;
+  second: boolean;
+}
+
+const hypotheses: {[token: string]: Hypothesis} = {};
+
+const variables: {[token: string]: null} = {};
+
+// An axiom or a theorem.
+class Assertion {
+    // Hypotheses of this axiom or theorem.
+    hypotheses: string[] = [];
+    disjvars: {[token: string]: [string, string]} = {};
+
+    // Statement of axiom or theorem.
+    expression: Expression = [];
+}
+
+const assertions: {[token: string]: Assertion} = {};
+
+class Scope {
+  activevariables: {[token: string]: null};
+  // Labels of active hypotheses
+  activehyp: string[] = [];
+  disjvars: {[token: string]: void}[] = [];
+  // Map from variable to label of active floating hypothesis
+  floatinghyp: {[token: string]: string} = {};
+}
+
+const scopes: Scope[] = [];
+
+const nProofCount: number = 0;
+const nProofLimit: number = Number.MAX_SAFE_INTEGER;
+
+// Determine if a string is used as a label
+function labelused(label: string): boolean {
+  return (hypotheses[label] || assertions[label]) ? true : false;
+}
+
+// Find active floating hypothesis corresponding to variable, or empty string
+// if there isn't one.
+function getfloatinghyp(variable: string): string {
+  for (let n = 0; n < scopes.length; ++n) {
+    const loc: string = scopes[n].floatinghyp[variable];
+    if (loc !== undefined) {
+      return loc;
+    }
+  }
+
+  return '';
+}
+
+function isactivevariable(str: string): boolean {
+  for (let n = 0; n < scopes.length; ++n) {
+    if (scopes[n].activevariables[str] !== undefined) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function isactivehyp(str: string): boolean {
+  for (let n = 0; n < scopes.length; ++n) {
+    if (scopes[n].activehyp[str] !== undefined) {
+      return true;
+    }
+  }
+  return false;
+}
+
