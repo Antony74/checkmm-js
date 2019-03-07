9225e4c181f53020255c6d18795bab93867901a2
Antony <akb@akb.me.uk>
2019-03-07 20:47:28 +0000

    Error handling

--- src/checkmmex.ts
+++ src/checkmmex.ts
@@ -13,83 +13,87 @@ export class CheckMMex extends CheckMM {
     this.mmFileNames.add(url);
 
     fetch(url).then((response: Response) => {
-      return response.text();
-    }).then((input: string) => {
-      let incomment = false;
-      let infileinclusion = false;
-      let newfilename: string = '';
+      if (!response.ok) {
+        callback('Failed to fetch "' + url + '": ' + response.statusText);
+      } else {
+        response.text().then((input: string) => {
+          let incomment = false;
+          let infileinclusion = false;
+          let newfilename: string = '';
 
-      let token: string = '';
-      while (true) {
-        ({token, input} = this.nexttoken(input));
-        if (!token.length) {
-          break;
-        }
+          let token: string = '';
+          while (true) {
+            ({token, input} = this.nexttoken(input));
+            if (!token.length) {
+              break;
+            }
 
-        if (incomment) {
-          if (token === '$)') {
-            incomment = false;
-            continue;
+            if (incomment) {
+              if (token === '$)') {
+                incomment = false;
+                continue;
+              }
+              if (token.indexOf('$(') !== -1) {
+                callback('Characters $( found in a comment');
+                return;
+              }
+              if (token.indexOf('$)') !== -1) {
+                callback('Characters $) found in a comment');
+                return;
+              }
+              continue;
+            }
+
+            // Not in comment
+            if (token === '$(') {
+                incomment = true;
+                continue;
+            }
+
+            if (infileinclusion) {
+              if (!newfilename.length) {
+                if (token.indexOf('$') !== -1) {
+                  callback('Filename ' + token + ' contains a $');
+                  return;
+                }
+                newfilename = token;
+                continue;
+              } else {
+                if (token !== '$]') {
+                  callback('Didn\'t find closing file inclusion delimiter');
+                  return;
+                }
+
+                const okay: boolean = this.readtokens(newfilename);
+                if (!okay) {
+                  return;
+                }
+                infileinclusion = false;
+                newfilename = '';
+                continue;
+              }
+            }
+            if (token === '$[') {
+              infileinclusion = true;
+              continue;
+            }
+
+            this.tokens.push(token);
           }
-          if (token.indexOf('$(') !== -1) {
-            callback('Characters $( found in a comment');
+
+          if (incomment) {
+            callback('Unclosed comment');
             return;
           }
-          if (token.indexOf('$)') !== -1) {
-            callback('Characters $) found in a comment');
+
+          if (infileinclusion) {
+            callback('Unfinished file inclusion command');
             return;
           }
-          continue;
-        }
 
-        // Not in comment
-        if (token === '$(') {
-            incomment = true;
-            continue;
-        }
-
-        if (infileinclusion) {
-          if (!newfilename.length) {
-            if (token.indexOf('$') !== -1) {
-              callback('Filename ' + token + ' contains a $');
-              return;
-            }
-            newfilename = token;
-            continue;
-          } else {
-            if (token !== '$]') {
-              callback('Didn\'t find closing file inclusion delimiter');
-              return;
-            }
-
-            const okay: boolean = this.readtokens(newfilename);
-            if (!okay) {
-              return;
-            }
-            infileinclusion = false;
-            newfilename = '';
-            continue;
-          }
-        }
-        if (token === '$[') {
-          infileinclusion = true;
-          continue;
-        }
-
-        this.tokens.push(token);
+          callback('');
+        });
       }
-
-      if (incomment) {
-        callback('Unclosed comment');
-        return;
-      }
-
-      if (infileinclusion) {
-        callback('Unfinished file inclusion command');
-        return;
-      }
-
-      callback('');
     });
 
   }
